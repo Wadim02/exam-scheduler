ad9dea126d9812208a8298bce09913707ade2e9d
Wadim02 <147058574+Wadim02@users.noreply.github.com>
2026-08-10 13:14:09 +0300

    clear old project
    clear repository

--- exam_app/create_tables.py
+++
@@ -1,5 +0,0 @@
-from models import Base, ExamenLimite, PropunereExamen, Subgrupe, Disciplina, Cadre, Facultati, Secretariat, Sefgrupe, Admin, Sali
-from database import engine
-
-Base.metadata.create_all(bind=engine)
-print("✔️ Tabelele au fost create.")
--- exam_app/email_utils.py
+++
@@ -1,5 +0,0 @@
-# exam_app/email_utils.py
-
-def trimite_email_sefi_grupa(emailuri: list[str]):
-    for email in emailuri:
-        print(f"[TEST] Trimitem email către {email} cu mesajul: 'Te rugăm să propui date pentru examene în platformă.'")
